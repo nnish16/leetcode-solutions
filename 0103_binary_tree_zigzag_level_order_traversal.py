175e2b77203cf7ae932726a71c07c7d5a649365c
Nishant <nnish1611@gmail.com>
2026-04-21 13:03:51 -0400

    Add LeetCode solutions for problems 102 and 103

---
+++ 0103_binary_tree_zigzag_level_order_traversal.py
@@ -0,0 +1,36 @@
+from collections import deque
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional["TreeNode"]) -> List[List[int]]:
+        if not root:
+            return []
+
+        result: List[List[int]] = []
+        queue = deque([root])
+        left_to_right = True
+
+        while queue:
+            level_size = len(queue)
+            level = deque()
+            for _ in range(level_size):
+                node = queue.popleft()
+                if left_to_right:
+                    level.append(node.val)
+                else:
+                    level.appendleft(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(list(level))
+            left_to_right = not left_to_right
+
+        return result
