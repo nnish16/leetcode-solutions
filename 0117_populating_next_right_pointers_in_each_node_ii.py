0a17df89f59ac4fe5f532e81f969b482a93249c8
Nishant <nnish1611@gmail.com>
2026-05-06 13:09:09 -0400

    Solve LeetCode 116 and 117

---
+++ 0117_populating_next_right_pointers_in_each_node_ii.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        current = root
+
+        while current:
+            dummy = Node(0)
+            tail = dummy
+
+            while current:
+                if current.left:
+                    tail.next = current.left
+                    tail = tail.next
+                if current.right:
+                    tail.next = current.right
+                    tail = tail.next
+                current = current.next
+
+            current = dummy.next
+
+        return root
